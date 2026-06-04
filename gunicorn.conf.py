35b936b79dadc509fcd59e2390cefb8a73a46279
yanj <yannicjae@icloud.com>
2026-06-04 21:38:55 +0200

    Fix WebSocket worker class and createRoom error handling

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,4 +1,4 @@
 workers = 1
-worker_class = "gevent"
+worker_class = "geventwebsocket.gunicorn.workers.GeventWebSocketWorker"
 worker_connections = 100
 bind = "0.0.0.0:" + __import__("os").environ.get("PORT", "8080")
